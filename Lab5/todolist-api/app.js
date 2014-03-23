d3e414583aef0795844741fa8bd44f1845655bc0
Jasper Poppe <jasperpoppe@msn.com>
2014-03-23 21:43:36 +0100

    work on lab5

--- Lab5/todolist-api/app.js
+++ Lab5/todolist-api/app.js
@@ -3,35 +3,18 @@ var todoApp = angular.module('todoApp', []);
 todoApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/EditTodo/:todoId', {
-	    templateUrl: 'templates/edit_todo.html',
-	    controller: 'EditTodoController'
-      }).
-      when('/', {
-        templateUrl: 'templates/index.html',
-        controller: 'TodoController'
-      });
+        when('/', {
+            templateUrl: 'templates/index.html',
+            controller: 'TodoController'
+        }).
+        when('/EditTodo/:todoId', {
+	       templateUrl: 'templates/edit_todo.html',
+	       controller: 'EditTodoController'
+        });
 }]);
  
 
 todoApp.controller('TodoController', function($scope, $http){
-	/*$scope.todos = [
-	    { id: 1,
-	      'what': 'A very urgent task', 
-	      'priority':'high', 
-	      'added_on': '2012-12-03 13:56:08'
-	    },
-	    { id: 2,
-	      'what': 'A normal priority task', 
-	      'priority':'normal', 
-	      'added_on': '2012-12-03 13:56:08'
-	    },
-	    { id: 3,
-	      'what': 'A low priority task', 
-	      'priority':'low', 
-	      'added_on': '2012-12-03 13:56:08'
-	    }
-	];*/
 
 	$scope.todos = [];
     $scope.getItems = function() {
@@ -39,7 +22,7 @@ todoApp.controller('TodoController', function($scope, $http){
         $http({
         	method : 'GET',
         	url : 'http://localhost:8888/web-mobile-labs/Lab5/todolist-api/web/todos',
-        	headers: { 
+        	headers: {
         		'X-Parse-Application-Id':'XXX', 
         		'X-Parse-REST-API-Key':'YYY'
         	}
@@ -51,14 +34,78 @@ todoApp.controller('TodoController', function($scope, $http){
             alert("Error");
         });
     };
+
+    $scope.saveNewTodo = function(){
+
+        $http({
+            url: "http://localhost:8888/web-mobile-labs/Lab5/todolist-api/web/todos/",
+            data: {
+                what: $scope.newtodo.what,
+                priority: $scope.newtodo.priority
+            },
+            method: 'POST',
+            headers: {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
+        }).success(function(data){
+            console.log("OK", data);
+        }).error(function(err){
+            console.log("ERR", err);
+        });
+    }
+
+    $scope.deleteTodo = function(todoId){
+        $http({
+            method : 'DELETE',
+            url : 'http://localhost:8888/web-mobile-labs/Lab5/todolist-api/web/todos/' + todoId,
+            headers: {
+                'X-Parse-Application-Id':'XXX', 
+                'X-Parse-REST-API-Key':'YYY'
+            }
+        })
+        .success(function(data, status) {
+            window.location.href = '#';
+        })
+        .error(function(data, status) {
+            alert("Error");
+        });
+    }
 });
 
 
-todoApp.controller('EditTodoController', function($scope, $routeParams) {
+todoApp.controller('EditTodoController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
 
-    $scope.todo_id = $routeParams.todoId;
-    $scope.todo_what = $routeParams.todoId;
-    $scope.todo_priority = $routeParams.todoId;
-    $scope.todo_added_on = $routeParams.todoId;
- 
-});
+    $scope.edittodo = [];
+    $scope.getItem = function() {
+
+        $http({
+            method : 'GET',
+            url : 'http://localhost:8888/web-mobile-labs/Lab5/todolist-api/web/todos/' + $routeParams.todoId,
+            headers: {
+                'X-Parse-Application-Id':'XXX', 
+                'X-Parse-REST-API-Key':'YYY'
+            }
+        })
+        .success(function(data, status) {
+            $scope.edittodo = data;
+        })
+        .error(function(data, status) {
+            alert("Error");
+        });
+    };
+
+    $scope.save = function(todoId){
+        $http({
+            url: "http://localhost:8888/web-mobile-labs/Lab5/todolist-api/web/todos/" + todoId,
+            data: {
+                what: $scope.edittodo.what,
+                priority: $scope.edittodo.priority
+            },
+            method: 'POST',
+            headers: {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
+        }).success(function(data){
+            console.log("OK", data);
+            window.location.href = '#';
+        }).error(function(err){
+            console.log("ERR", err);
+        });
+    }
+}]);
